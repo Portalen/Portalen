5db211517e62d2261de3de9ed9c38b1eff9a9cae
Jonas Jongejan <jonas@halfdanj.dk>
2013-05-02 17:54:33 +0200

    Stream and receive in one app

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,17 +2,45 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
+    streamerSend = new ofxStreamerSender();
+    streamerSend->setup(640, 480, "127.0.0.1", 8888);
+    
+    streamerRecv = new ofxStreamerReceiver();
+    streamerRecv->setup(8888);
+    
+    grabber = new ofVideoGrabber();
+    grabber->initGrabber(640, 480);
+    
+    data = (unsigned char*) malloc(sizeof(char)* 640 * 480 * 3*10);
 
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
+    grabber->update();
+    
+    if(grabber->isFrameNew()){
+        
+        ofBuffer buffer;
+        buffer.set((char*)data, 640 * 480 * 3);
+        
+        inputImage.setFromPixels(data, 640, 480, OF_IMAGE_COLOR);
+        
+        streamerSend->encodeFrame(grabber->getPixels(),  640 * 480 * 3);
+        streamerSend->sendFrame();
+    }
+    
+    
+    streamerRecv->update();
 
+    
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    grabber->draw(0, 0, 640, 480);
+    
+    streamerRecv->draw(640, 0, 640, 480);
 }
 
 //--------------------------------------------------------------
