f13b32755c926db0e05490bd713e323ec3438361
Johan Bichel Lindegaard <public@johan.cc>
2013-05-03 14:09:55 +0200

    Added shards.

---
+++ src/shards.cpp
@@ -0,0 +1,9 @@
+//
+//  shards.cpp
+//  Portalen
+//
+//  Created by Johan Bichel Lindegaard on 5/3/13.
+//
+//
+
+#include "shards.h"
---
+++ src/shards.h
@@ -0,0 +1,14 @@
+//
+//  shards.h
+//  Portalen
+//
+//  Created by Johan Bichel Lindegaard on 5/3/13.
+//
+//
+
+#ifndef Portalen_shards_h
+#define Portalen_shards_h
+
+
+
+#endif
