7b047806aac4cf1aa90bd686913d35160dd3b406
Johan Bichel Lindegaard <johan@johan.cc>
2014-10-13 16:00:31 +0200

    Dual streams. Canon EDSK.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,32 +3,54 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    ofLogLevel(OF_LOG_WARNING);
+    canon.start();
+
+    //ofLogLevel(OF_LOG_SILENT);
     ofSetFrameRate(30);
     
-    grabber.setDesiredFrameRate(2);
+    //grabber.setDesiredFrameRate(2);
+    //grabber.initGrabber(1920/2, 1080/2);
     
-    grabber.initGrabber(1920/2, 1080/2);
+    streamWidth = 1056;
+    streamHeight = 704;
     
-    sender.setup(grabber.getWidth(), grabber.getHeight(), REMOTE_HOST);
-    camOutFbo.allocate(grabber.getWidth(), grabber.getHeight());
-    outputFbo.allocate(grabber.getWidth(), grabber.getHeight());
+    roiMaxRadius = 200;
     
-    receiver.setup(1234);
+    hqsender.setup(roiMaxRadius, roiMaxRadius, REMOTE_HOST, 1234);//, "placebo", "zerolatency");
+    lqsender.setup(streamWidth/4, streamHeight/4, REMOTE_HOST, 1235);//, "ultrafast", "zerolatency");
+    
+    camFbo.allocate(streamWidth, streamHeight, GL_RGB8);
+    
+    camOutFboHQ.allocate(roiMaxRadius, roiMaxRadius, GL_RGB8);
+    camOutFboLQ.allocate(streamWidth/4, streamHeight/4, GL_RGB8);
 
+    hqreceiver.setup(1234);
+    lqreceiver.setup(1235);
+    
+    camFbo.begin();
+    ofClear(0,0,0);
+    camFbo.end();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    grabber.update();
-    receiver.update();
+    //grabber.update();
+    lqreceiver.update();
+    hqreceiver.update();
     
-    if(grabber.isInitialized() && grabber.isFrameNew()){
+    if(!canon.isLiveViewActive() && canon.isSessionOpen()) {
+        canon.startLiveView();
+    }
+    
+    //if(grabber.isInitialized() && grabber.isFrameNew()){
         
-        newframes++;
+       // newframes++;
         
         /*camOutFbo.begin();
+         canon.drawLiveView();
+
         grabber.draw(0, 0);
         //ofBackground(0, 0, 0, 100);
         
@@ -42,29 +64,73 @@ void ofApp::update(){
         
         //sender.sendFrame(outPixels); //
         
-        if(newframes > 20) {
+        
+        
+       /* if(newframes > 20) {
             newframes = 0;
-        }
-    }
+        }*/
+    //}
     
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    
-    sender.sendFrame(grabber.getPixelsRef());
+    ofSetWindowTitle(ofToString(ofGetFrameRate()));
+    //sender.sendFrame(grabber.getPixelsRef());
     
     //outputFbo.begin();
     //outputFbo.end();
+    ofSetColor(255, 255, 255, 255);
+    //grabber.draw(0, 0);
+    if(canon.isLiveViewActive()) {
+        
+        camFbo.begin();
+        canon.drawLiveView();
+        camFbo.end();
+        
+        camOutFboHQ.begin();
+        camFbo.draw(roi.center);
+        camOutFboHQ.end();
+        
+        camOutFboLQ.begin();
+        camFbo.draw(0,0,camOutFboLQ.getWidth(),camOutFboLQ.getHeight());
+        camOutFboLQ.end();
+    }
     
-    grabber.draw(0, 0);
+    camOutFboLQ.readToPixels(outPixelsLQ);
+    lqsender.sendFrame(outPixelsLQ);
+    
+    float timePerFrame = 1.0 / hqFrameRate;
+    float currentTime = ofGetElapsedTimef();
+    if (currentTime - lastTime > timePerFrame){
+        
+        camOutFboHQ.readToPixels(outPixelsHQ);
+        hqsender.sendFrame(outPixelsHQ);
+        lastTime = currentTime;
+    }  
+    
+    camFbo.draw(0,0,streamWidth/2, streamHeight/2);
+    camOutFboHQ.draw(0,streamHeight/2,camOutFboHQ.getWidth(),camOutFboHQ.getHeight());
+    
+    ofPushMatrix();
+    
+    ofTranslate(streamWidth/2,  0);
+    lqreceiver.draw(0, 0, streamWidth/2  , streamHeight/2 );
+    ofSetColor(255,255,255,255);
+    hqreceiver.draw(0, 0, roiMaxRadius, roiMaxRadius);
+    
+    ofPopMatrix();
 
-    receiver.draw(0, 400);
+    //cout<<receiver.isConnected()<<endl;
+    
+    
+    
 
 }
 
 void ofApp::exit(){
-    receiver.close();
+    canon.endLiveView();
+    //receiver.close();
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,8 +3,17 @@
 #include "ofMain.h"
 #include "ofxStreamer.h"
 #include "ofxOpenCv.h"
+#include "Canon.h"
+#include "ofxOsc.h"
 
 #define REMOTE_HOST "127.0.0.1"
+
+class RegionOfInterest {
+public:
+    ofVec2f center;
+    float radius;
+    float zoom;
+};
 
 class ofApp : public ofBaseApp{
 
@@ -25,16 +34,37 @@ class ofApp : public ofBaseApp{
     
     
     ofVideoGrabber grabber;
-    ofxStreamerReceiver receiver;
-    ofxStreamerSender sender;
     
-    ofFbo camOutFbo;
+    ofxOscReceiver oscReciver;
+    ofxOscSender oscSender;
+    
+    ofxStreamerReceiver hqreceiver;
+    ofxStreamerReceiver lqreceiver;
+    
+    ofxStreamerSender hqsender;
+    ofxStreamerSender lqsender;
+    
+    float hqFrameRate = 25;
+    
+    float streamWidth, streamHeight;
+    
+    ofFbo camFbo;
+    ofFbo camOutFboHQ;
+    ofFbo camOutFboLQ;
     ofFbo outputFbo;
     
-    ofPixels outPixels;
+    ofPixels outPixelsHQ;
+    ofPixels outPixelsLQ;
     ofxCvColorImage image;
     
-    
     int newframes = 0;
+    
+    roxlu::Canon canon;
+    
+    float lastTime;
+    
+    RegionOfInterest roi;
+    
+    float roiMaxRadius;
     
 };
